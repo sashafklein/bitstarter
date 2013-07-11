837e215c5c2bdeb16066acd66f3563f68a6b9cec
Ubuntu <ubuntu@ip-172-31-38-98.us-west-2.compute.internal>
2013-07-11 06:47:25 +0000

    full path

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  data = fs.readFileSync('/index.html');
+ data = fs.readFileSync('/home/ubuntu/bitstarter/index.html');
   response.send(data); 
 });
 
