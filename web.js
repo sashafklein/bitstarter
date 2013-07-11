78cadf94515fd7c82b5c336ff6528cc7125a66e1
Ubuntu <ubuntu@ip-172-31-38-98.us-west-2.compute.internal>
2013-07-11 06:52:51 +0000

    parenthesis

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   data = fs.readFileSync('index.html');
-  response.send(data.toString); 
+  response.send(data.toString()); 
 });
 
 var port = process.env.PORT || 5000;
