7a4a3284299c7d73e4cce4eedf6f456851a52752
Sasha Klein <sashafklein@gmail.com>
2013-07-15 22:23:36 -0700

    Port 3000

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ app.get('/', function(request, response) {
   response.send(data.toString()); 
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
